3ee907e4b9a438084dada4347652980c5d725841
Samuel <samuel.v.soriano@gmail.com>
2024-01-20 16:43:57 -0700

    Added mobile friendly version of the categories tab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,15 +26,39 @@
 		<img class="h-full w-auto" src="/assets/logo/horizontal-gradient.svg" alt="AI FLUENTLY logo" />
 	</div>
 </header>
-<div class="grid justify-center m-10">
-	<div class="btn-group variant-filled text-xl">
-		<button on:click={() => (category = 1)}>Main</button>
-		<button on:click={() => (category = 2)}>Business</button>
-		<button on:click={() => (category = 3)}>Education</button>
-		<button on:click={() => (category = 4)}>Entertainment</button>
-		<button on:click={() => (category = 5)}>Technology</button>
-		<button on:click={() => (category = 6)}>Creative</button>
-		<button on:click={() => (category = 7)}>Lifestyle</button>
+
+<div class="grid justify-center w-full">
+	<div
+		class="snap-x scroll-px-4 snap-mandatory no-scrollbar scroll-smooth flex gap-4 overflow-x-auto px-4 py-10"
+	>
+		<button
+			class="snap-start shrink-0 card py-2 w-32 md:w-32 text-center"
+			on:click={() => (category = 1)}>Main</button
+		>
+		<button
+			class="snap-start shrink-0 card py-2 w-32 md:w-32 text-center"
+			on:click={() => (category = 2)}>Business</button
+		>
+		<button
+			class="snap-start shrink-0 card py-2 w-32 md:w-32 text-center"
+			on:click={() => (category = 3)}>Education</button
+		>
+		<button
+			class="snap-start shrink-0 card py-2 w-32 md:w-32 text-center"
+			on:click={() => (category = 4)}>Entertainment</button
+		>
+		<button
+			class="snap-start shrink-0 card py-2 w-32 md:w-32 text-center"
+			on:click={() => (category = 5)}>Technology</button
+		>
+		<button
+			class="snap-start shrink-0 card py-2 w-32 md:w-32 text-center"
+			on:click={() => (category = 6)}>Creative</button
+		>
+		<button
+			class="snap-start shrink-0 card py-2 w-32 md:w-32 text-center"
+			on:click={() => (category = 7)}>Lifestyle</button
+		>
 	</div>
 </div>
 
@@ -67,54 +91,179 @@
 
 {#if category === 2}
 	<div in:fade={fadeOptions}>
-		<BentogridB {category} {intersecting} />
-		<BentogridB2 {category} {intersecting} />
 		<BentogridA {category} {intersecting} />
-		<BentogridB3 {category} {intersecting} />
+
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB2 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB3 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
 	</div>
 {/if}
 
 {#if category === 3}
 	<div in:fade={fadeOptions}>
 		<BentogridA {category} {intersecting} />
-		<BentogridB {category} {intersecting} />
-		<BentogridB2 {category} {intersecting} />
-		<BentogridB3 {category} {intersecting} />
+
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB2 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB3 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
 	</div>
 {/if}
 
 {#if category === 4}
 	<div in:fade={fadeOptions}>
 		<BentogridA {category} {intersecting} />
-		<BentogridB {category} {intersecting} />
-		<BentogridB2 {category} {intersecting} />
-		<BentogridB3 {category} {intersecting} />
+
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB2 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB3 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
 	</div>
 {/if}
 
 {#if category === 5}
 	<div in:fade={fadeOptions}>
 		<BentogridA {category} {intersecting} />
-		<BentogridB {category} {intersecting} />
-		<BentogridB2 {category} {intersecting} />
-		<BentogridB3 {category} {intersecting} />
+
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB2 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB3 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
 	</div>
 {/if}
 
 {#if category === 6}
 	<div in:fade={fadeOptions}>
 		<BentogridA {category} {intersecting} />
-		<BentogridB {category} {intersecting} />
-		<BentogridB2 {category} {intersecting} />
-		<BentogridB3 {category} {intersecting} />
+
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB2 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB3 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
 	</div>
 {/if}
 
 {#if category === 7}
 	<div in:fade={fadeOptions}>
 		<BentogridA {category} {intersecting} />
-		<BentogridB {category} {intersecting} />
-		<BentogridB2 {category} {intersecting} />
-		<BentogridB3 {category} {intersecting} />
+
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB2 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
+		<IntersectionObserver let:intersecting once={true}>
+			{#if intersecting || nativeLoading}
+				<div>
+					<BentogridB3 {category} {intersecting} />
+				</div>
+			{/if}
+		</IntersectionObserver>
 	</div>
 {/if}
+
+<style>
+	/* Custom styles for removing scrollbar visibility */
+	.no-scrollbar::-webkit-scrollbar {
+		display: none; /* for Safari, Chrome, Opera */
+	}
+	.no-scrollbar {
+		-ms-overflow-style: none; /* for Internet Explorer, Edge */
+		scrollbar-width: none; /* for Firefox */
+	}
+</style>
