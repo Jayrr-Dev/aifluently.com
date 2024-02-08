383dc5f7d93dbe79f069cd1de9ddb4c3d468df3b
Samuel <samuel.v.soriano@gmail.com>
2024-02-08 12:15:56 -0700

    fix

--- src/lib/ui/cardlist.svelte
+++ src/lib/ui/cardlist.svelte
@@ -268,20 +268,5 @@
 		</button>
 	</div>
 {:else}
-	<div class="card h-[{outer_height}] grid grid-cols-1 relative card-hover">
-		<div class="p-4 space-y-4">
-			<div class="placeholder animate-pulse" />
-			<div class="grid grid-cols-3 gap-8">
-				<div class="placeholder animate-pulse" />
-				<div class="placeholder animate-pulse" />
-				<div class="placeholder animate-pulse" />
-			</div>
-			<div class="grid grid-cols-4 gap-4">
-				<div class="placeholder animate-pulse" />
-				<div class="placeholder animate-pulse" />
-				<div class="placeholder animate-pulse" />
-				<div class="placeholder animate-pulse" />
-			</div>
-		</div>
-	</div>
+	<div class="card h-[{outer_height}] grid grid-cols-1 relative card-hover"></div>
 {/if}
